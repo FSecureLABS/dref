226917ad600a56dbb24f0ca8038a708015606fb5
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-07-19 15:46:02 +0100

    close connection on /checkpoint

--- dref/api/src/routes/checkpoint.js
+++ dref/api/src/routes/checkpoint.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 var router = Router()
 
 router.get('/', function (req, res, next) {
+  res.set('Connection', 'close')
   res.send(JSON.stringify({
     checkpoint: true
   }))
