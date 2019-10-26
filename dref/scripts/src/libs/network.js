dfff9518813b1064c4922a3c4983ca7953df5259
serain <alex@kaskaso.li>
2019-10-26 23:33:01 +0100

    remove reserver hosts

--- dref/scripts/src/libs/network.js
+++ dref/scripts/src/libs/network.js
@@ -129,5 +129,5 @@ export async function getLocalSubnet (suffix = 24) {
   if (localIp === '') return []
 
   const cidr = new IPCIDR(localIp + '/' + suffix)
-  return cidr.toArray()
+  return cidr.toArray().pop().shift()
 }
